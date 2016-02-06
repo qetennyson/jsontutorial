f13f61dd80a5e823aaaa774d5a6f9b92980189a5
qetennyson <quincytennyson@yahoo.com>
2016-02-05 22:42:57 -0500

    added tutorials.json

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsontutorialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jsontutorialApp
+ */
+angular.module('jsontutorialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsontutorialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jsontutorialApp
+ */
+angular.module('jsontutorialApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
