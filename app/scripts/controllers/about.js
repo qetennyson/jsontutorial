98de12085344a00a62c9089dd53e184fe01f2b71
qetennyson <quincytennyson@yahoo.com>
2016-02-08 20:44:33 -0500

    trying again

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -12,5 +12,5 @@ angular.module('jsontutorialApp')
         $scope.isActive = function (viewlocation) {
             return viewlocation === $location.path();
         };
-    ];
-  });
+    
+  }]);
